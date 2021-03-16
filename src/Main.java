a33b2fddaed39eda7c76a6c31d892fa82ec7d90e
Roman Melnikov <Romaneasyrun@yandex.ru>
2021-03-16 21:22:57 +0700

    com 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 14000;
+        int condition = 20;
+        int bonus = cost / condition;
+        System.out.println(bonus);
+
+    }
+}
